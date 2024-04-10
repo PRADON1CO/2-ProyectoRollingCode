f5e5335d2d31ddfc3d0fc8a145c3891189592784
Claudia Torres <alet24665@gmail.com>
2024-04-10 20:34:28 -0300

    cree la funcion altaJuego para administrar la logica de create

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
 const btnAgregarJuego = document.getElementById("btnCrearJuego");
 const modalJuego = new bootstrap.Modal(document.getElementById("modalJuego"));
+let agregarJuego = true;
+const formJuego = document.getElementById("formJuego");
 
-const mostrarModalJuego = () => {
+function mostrarModalJuego() {
+  agregarJuego = true;
   modalJuego.show();
-};
+}
+
+function administrarFormJuego(e) {
+  e.preventDefault();
+  if (agregarJuego === true) {
+    altaJuego();
+  } else {
+  }
+}
+
+function altaJuego() {}
 
 btnAgregarJuego.addEventListener("click", mostrarModalJuego);
+formJuego.addEventListener("submit", administrarFormJuego);
