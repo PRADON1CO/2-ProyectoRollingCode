7a99c3cf7ee1b808f10b947d792ca5fa1b10870e
Claudia Torres <alet24665@gmail.com>
2024-04-11 12:36:09 -0300

    use prevetDefault para que la pagina no se recargue automaticamente

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 const btnAgregarJuego = document.getElementById("btnAgregarJuego");
 const modalJuego = new bootstrap.Modal(document.getElementById("modalJuego"));
+let crearJuego = true;
+const formJuego = document.getElementById("formJuego");
 
 function mostrarModalJuego() {
+  crearJuego = true;
   modalJuego.show();
 }
 
+function administrarFormJuego(e) {
+  e.preventDefault();
+}
+
+
 btnAgregarJuego.addEventListener("click", mostrarModalJuego);
+formJuego.addEventListener("submit", administrarFormJuego);
