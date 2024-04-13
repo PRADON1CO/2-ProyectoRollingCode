5cc38d69aa7fae95b8211f6ed8a7094d5386b252
Esteban Lopez <lopezestebanalejandro@gmail.com>
2024-04-13 12:36:33 -0300

    se agregar bloque de comentarios, ademas se vincula los botons de responder editar y borrar con la paginas de error

--- js/detalleJuego.js
+++ js/detalleJuego.js
@@ -1,12 +1,14 @@
 const parametrosURL = new URLSearchParams(window.location.search);
-const codigoPelicula = parametrosURL.get('codigo')
+const codigoPelicula = parametrosURL.get("codigo");
 
-const listaJuegos = JSON.parse(localStorage.getItem('listaJuegosKey')) || [];
+const listaJuegos = JSON.parse(localStorage.getItem("listaJuegosKey")) || [];
 
-const juegoBuscada = listaJuegos.find((itemJuego)=> itemJuego.codigo === codigoPelicula)
+const juegoBuscada = listaJuegos.find(
+  (itemJuego) => itemJuego.codigo === codigoPelicula
+);
 
-//dibujar la card 
-const seccionDetalle = document.querySelector('#seccionDetalle')
+//dibujar la card
+const seccionDetalle = document.querySelector("#seccionDetalle");
 seccionDetalle.innerHTML = `<div class="card mb-3">
 <div class="row g-0">
   <div class="col-md-4">
@@ -31,4 +33,24 @@ seccionDetalle.innerHTML = `<div class="card mb-3">
     </div>
   </div>
 </div>
-</div>`
+
+
+  <section class="container my-5">
+  <p class="display-6">Reseñas</p>
+      <hr />
+                  <img src="../img/EstebanLopez.jpg" width="64" height="64" alt="Comentario de usuario">
+                  <article class="media-body">
+                      <p class="nombre">Rafael<span>15:52 pm, Hoy</span></p>
+                      <p class="comentario">Desde el momento en que inicié mi primera partida, 
+                        quedé impresionado por la calidad visual de <span class="badge rounded-pill bg-dark"> ${juegoBuscada.nombre}</span>.
+                        Este juego ha recibido críticas impresionantes. La meticulosa atención a los detalles, desde los entornos hasta 
+                        los personajes, crea una experiencia envolvente que realmente se siente auténtica.</p>
+                  </article>
+                  <div class="botones text-right">
+                      <a href="/pages/error.html">Responder</a>
+                      <a href="/pages/error.html">Editar</a>
+                      <a href="/pages/error.html">Borrar</a>
+                  </div>
+  </section>
+
+</div>`;
